3acb7fc3006188e755ffda2aa0296645a6434c0d
den-taku <dentaku1412@gmail.com>
2021-07-03 08:41:47 +0900

    Add \n

--- cpp_11_14_17/block.cpp
+++ cpp_11_14_17/block.cpp
@@ -3,5 +3,7 @@
 int main() {
     int num = 4;
     if (num > 3)
-        printf("Hello, "), printf("World!"), printf("\n");
+        printf("Hello, "),
+        printf("World!"), 
+        printf("\n");
 }
